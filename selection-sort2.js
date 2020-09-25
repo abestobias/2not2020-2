7cd9c96dc5b637cd82e39bc166e704db2e362600
abestobias <aytobias@yahoo.com.br>
2020-09-25 00:51:51 +0000

    selection-sort( 24/09)

--- selection-sort2.js
+++ selection-sort2.js
@@ -1,4 +1,12 @@
-function selectionSort(vetor) {
+/* 
+Função de comparação fnComp
+- Recebe dois valores para a comparação 
+- Retorno:
+    -True se o primeiro for maior que o segundo
+    -False caso contrario
+*/
+
+function selectionSort(vetor, fnComp) {
     let pasadas = 0, comparacoes = 0, totalTrocas = 0
 
 
@@ -7,7 +15,8 @@ function selectionSort(vetor) {
     function encontrarMenor(vetor, inicio) {
         let posMenor = inicio
         for (let i = inicio + 1; i < vetor.length; i++) {
-            if (vetor[i] < vetor[posMenor]) posMenor = i
+            //if(vetor[i] < vetor[posMenor]) posMenor =i
+            if (! fnComp(vetor[i],  vetor[posMenor])) posMenor = i
             comparacoes++
         }
         return posMenor
@@ -22,7 +31,8 @@ function selectionSort(vetor) {
         // Caso o valor encontrado seja menor que o valor atual,
         //procede-se a troca
         comparacoes++
-        if (vetor[posMenor] < vetor[i]) {
+        //if (vetor[posMenor] < vetor[i]) {
+        if (fnComp( vetor[i], vetor[posMenor])) { // ordem dos argumentos invertidos
             // Permuta de valores usando a desetruturação 
             [vetor[posMenor], vetor[i]] = [vetor[i], vetor[posMenor]]
             totalTrocas++
@@ -31,25 +41,17 @@ function selectionSort(vetor) {
     console.log({ pasadas, comparacoes, totalTrocas })
 }
 
-/*const nums =[56, 78, 44, 23, 99, 14, 60, 37, 6, 82, 31, 65]
 
-console.time('nums')
-selectionSort(nums)
-console.timeEnd('nums')
 
-console.log(nums)
+let candidatos = require('./dados/candidatos-2018')
 
-*/
-
-let nomes = require('./dados/100-mil-nomes')
-
-console.time('Teste Nomes')
-selectionSort(nomes)
-console.timeEnd('Teste Nomes')
+console.time('Teste candidatos-2018')
+selectionSort(candidatos, (x, y) => x.NM_CANDIDATO > y.NM_CANDIDATO)
+console.timeEnd('Teste candidatos-2018')
 
 // medindo a memoria utilizada pelo programa 
 
 let memoria= process.memoryUsage().heapUsed /1024 /1024 
-console.log(nomes)
+console.log(candidatos)
 console.log(' Memoria utilizada (MB):', memoria)
 
